282f5e6e86fa7a1bd59d16df93bcdcf95066d2b0
Tin Goricki <exibodude@gmail.com>
2023-04-01 23:16:18 +0200

    Added cart with useContext to CartPage

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { CartContext } from "../Components/RouteSwitch";
+
 const CartPage = () => {
-    
+    const cart = useContext(CartContext)
+
     return (
         <div>
             <p>This is the cart Page</p>
